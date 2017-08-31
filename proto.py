a4162871db030749191e1b46d43f6270e3851c00
codehorsey <codehorsey@gmail.com>
2017-08-30 19:38:44 -0500

    unload_pockets

--- proto.py
+++ proto.py
@@ -4,6 +4,26 @@ logger = logging.getLogger(__name__)
 
 class Person():
 
+    def __init__(self):
+        pass
+
+    def pickup_thing(self, thing):
+        self.pockets = thing
+        logger.debug('Person picked up {}.'.format(thing))
+    
+    def unload_pockets(self, truck):
+        logger.info("Person unloading pockets")
+        if isinstance(truck, Truck) and truck.eligable_thing(self.pockets):
+                logger.debug("Truck accepts {}".format(self.pockets))
+                truck.take_thing(self.pockets)
+                self.pockets = None
+                logger.debug("Pockets are empty")
+        elif not isinstance(truck, Truck):
+            logger.error("Wrong class! {} not accepted".format(truck.__class__))
+        else:
+            logger.debug("Truck does not accept {}".format(self.pockets))
+
+
     def unload_thing(self, thing, truck):
         """Attempt to unload thing onto a truck.
         
@@ -15,8 +35,9 @@ class Person():
         if isinstance(truck, Truck) and truck.eligable_thing(thing):
                 logger.debug("Truck accepts {}".format(thing))
                 truck.take_thing(thing)
+                
         elif not isinstance(truck, Truck):
-        	logger.error("Wrong class! {} not accepted".format(truck.__class__))
+            logger.error("Wrong class! {} not accepted".format(truck.__class__))
         else:
             logger.debug("Truck does not accept {}".format(thing))
 
@@ -47,10 +68,10 @@ class Truck():
         self.accepts[thing] -= 1
         logger.debug('Added {} to truck inventory. Room for {} more.'.format(thing, self.accepts[thing]))
 
-accepts = ['apple', 'bannana','carrot']
 normal_load = {'apple':3,'carrot':1}
+heavy_load = {'watermelon':3}
 t = Truck(normal_load)
-d = Person()
+d = Truck(heavy_load)
 p = Person()
 
 p.unload_thing('ban', t)
@@ -61,5 +82,9 @@ p.unload_thing('apple', t)
 p.unload_thing('apple', t)
 p.unload_thing('apple', d)
 
+p.pickup_thing('watermelon')
+p.unload_pockets(t)
+p.unload_pockets(d)
 
+print p.pockets
 print t.inventory
